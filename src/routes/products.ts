172c8f7c7ab511f297a6b1f22cebd62e0e251a0d
gildembergleite <gildembergleite@gmail.com>
2024-10-09 10:52:50 -0300

    refactor: add new error responses

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -5,9 +5,34 @@ import { Router } from "express";
 const prisma = new PrismaClient()
 const router = Router()
 
-router.get('/', async (_, res) => {
-  const products = await prisma.product.findMany()
-  res.json(products)
+router.get('/', async (req, res) => {
+  const { page = '1', page_size = '10' } = req.query
+  
+  const pageNumber = parseInt(page as string)
+  const pageSizeNumber = parseInt(page_size as string)
+  const skip = (pageNumber - 1) * pageSizeNumber
+
+  try {
+    const products = await prisma.product.findMany({
+      skip,
+      take: pageSizeNumber
+    })
+
+    const count = await prisma.product.count()
+    const totalPages = Math.ceil(count / pageSizeNumber)
+
+    res.json({
+      count,
+      total_pages: totalPages,
+      page_size: pageSizeNumber,
+      page: pageNumber,
+      results: products
+    })
+    
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Não foi possível listar os produtos' })
+  }
 })
 
 router.get('/:id', async (req, res) => {
@@ -27,6 +52,21 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const { name, category, price } = req.body
+
+  if (!name) {
+    res.status(400).json({ error: 'O campo [name] é obrigatório' })
+    return
+  }
+
+  if (!category) {
+    res.status(400).json({ error: 'O campo [category] é obrigatório' })
+    return
+  }
+
+  if (!price) {
+    res.status(400).json({ error: 'O campo [price] é obrigatório' })
+    return
+  }
 
   try {
     const product = await prisma.product.create({
@@ -49,7 +89,13 @@ router.patch('/:id', async (req, res) => {
       where: { id: Number(id) },
       data: { name, category, price: parseFloat(price) }
     })
-    res.json(product)
+
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(400).json({ error: 'Não foi localizado nenhum produto com o id informado' })
+    }
+
   } catch (error) {
     console.error(error)
     res.status(400).json({ error: 'Não foi possível atualizar o produto' })
@@ -63,7 +109,12 @@ router.delete('/:id', async (req, res) => {
     const product = await prisma.product.delete({
       where: { id: Number(id) }
     })
-    res.status(204).end()
+
+    if (product) {
+      res.status(204).json(product)
+    } else {
+      res.status(400).json({ error: 'Não foi localizado nenhum produto com o id informado' })
+    }
   } catch (error) {
     console.error(error)
     res.status(400).json({ error: 'Não foi possível deletar o produto' })
