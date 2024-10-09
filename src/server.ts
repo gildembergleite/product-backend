817f0fb64a598add64efe9f8de7f6bef97d6cc96
gildembergleite <gildembergleite@gmail.com>
2024-10-09 19:39:00 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import { productRouter } from './routes/products'
@@ -6,8 +7,9 @@ import { setupSwagger } from './swagger'
 dotenv.config()
 
 const app = express()
-app.use(express.json())
 
+app.use(express.json())
+app.use(cors())
 app.use('/api/products', productRouter)
 
 setupSwagger(app)
