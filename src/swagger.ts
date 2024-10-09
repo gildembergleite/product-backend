43ac5e9e942744ad1e6fa6d0a15f5d8a510c9b26
gildembergleite <gildembergleite@gmail.com>
2024-10-09 11:01:30 -0300

    feat: configure swagger

---
+++ src/swagger.ts
@@ -0,0 +1,26 @@
+import { Express } from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Product API',
+      version: '1.0.0',
+      description: 'A simple API for managing products',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export function setupSwagger(app: Express) {
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
