7c64761a3e3d0218c6b3be1a79ff3110b846e33d
gildembergleite <gildembergleite@gmail.com>
2024-10-09 11:27:31 -0300

    feat: configure server url in env

--- src/swagger.ts
+++ src/swagger.ts
@@ -2,6 +2,8 @@ import { Express } from 'express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
+const url = process.env.SERVER_URL || 'http://localhost:3333/api';
+
 const options = {
   definition: {
     openapi: '3.0.0',
@@ -10,11 +12,7 @@ const options = {
       version: '1.0.0',
       description: 'A simple API for managing products',
     },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-      },
-    ],
+    servers: [{ url }],
   },
   apis: ['./src/routes/*.ts'],
 };
@@ -23,4 +21,9 @@ const swaggerSpec = swaggerJsdoc(options);
 
 export function setupSwagger(app: Express) {
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+  app.get('/api.json', (_, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
 }
